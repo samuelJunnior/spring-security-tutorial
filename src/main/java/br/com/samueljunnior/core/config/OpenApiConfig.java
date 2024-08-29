ddf9a5c066f3cfc8442f9d03f1041049d10029da
samuel <cast.samuel.uitc@sebrae.com.br>
2024-08-29 20:01:51 -0300

    ajustes criação de usuário e documentação swagger.

--- src/main/java/br/com/samueljunnior/core/config/OpenApiConfig.java
+++ src/main/java/br/com/samueljunnior/core/config/OpenApiConfig.java
@@ -2,6 +2,8 @@ package br.com.samueljunnior.core.config;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.customizers.OpenApiCustomiser;
@@ -14,15 +16,33 @@ public class OpenApiConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
+                .info(new Info()
+                        .title("API de Exemplo com OAuth2")
+                        .contact(new Contact()
+                                .name("Samuel Junnior")
+                                .url("https://samueljunnior.github.io/about-me/")
+                        ).version("v1")
+                )
                 .components(new Components()
                         .addSecuritySchemes("bearer-key",
-                                new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")))
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        ))
                 .addSecurityItem(new SecurityRequirement().addList("bearer-key"));
     }
 
     @Bean
     public OpenApiCustomiser customerGlobalHeaderOpenApiCustomiser() {
-        return openApi -> openApi.getPaths().values().forEach(pathItem -> pathItem.readOperations().forEach(operation -> operation.addSecurityItem(new SecurityRequirement().addList("bearer-key"))));
+        return openApi -> openApi
+                .getPaths().values()
+                .forEach(pathItem ->
+                        pathItem.readOperations()
+                                .forEach(operation ->
+                                        operation.addSecurityItem(new SecurityRequirement().addList("bearer-key"))
+                                )
+                );
     }
 
 }
--- src/main/java/br/com/samueljunnior/modules/user/controller/UserController.java
+++ src/main/java/br/com/samueljunnior/modules/user/controller/UserController.java
@@ -14,16 +14,18 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/users")
 @Tag(
         name = "Users",
-        description = "Operações sobre operações de Usuário.",
+        description = "Operações sobre Usuário.",
         externalDocs = @ExternalDocumentation(
                 description = "Developer Website",
                 url = "https://samueljunnior.github.io/about-me/"
@@ -33,7 +35,7 @@ public class UserController {
 
     private final UserService userService;
 
-    @PostMapping
+    @PostMapping("/public/users")
     @Operation(
             summary = "Realizar criação de nova usuário.",
             description = "Realizar criação de nova psotagem.",
@@ -50,11 +52,11 @@ public class UserController {
         return ResponseEntity.ok(userService.newUser(userDto));
     }
 
-    @GetMapping
+    @GetMapping("/users")
     @PreAuthorize("hasRole('ADMIN')")
     @Operation(
             summary = "Realizar busca de todos usuários.",
-            description = "Realizar busca de todos usuários.",
+            description = "Realizar busca de todos usuários - apenas para usuário admin.",
             responses = {
                     @ApiResponse(
                             responseCode = "200",
